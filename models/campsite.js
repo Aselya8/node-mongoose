50c51e5296fb755b4b1caaa00382cc8675c1d625
Asel Atakeev <aselya.atakeeva@gmail.com>
2021-11-12 23:58:18 -0800

    "Mongoose Part 3"

--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//added commentSchema
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 const campsiteSchema = new Schema({
     name: {
         type: String,
@@ -10,10 +30,12 @@ const campsiteSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]//added comentSchema
 }, {
     timestamps: true
 });
+
 
 const Campsite = mongoose.model('Campsite', campsiteSchema);
 
